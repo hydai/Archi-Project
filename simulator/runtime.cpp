4b1ebf3e3c01800c0cdcd119c9348141463ac8cb
hydai <z54981220@gmail.com>
2014-03-04 00:19:11 +0800

    Add runtime to Makefile

--- simulator/runtime.cpp
+++ simulator/runtime.cpp
@@ -9,64 +9,32 @@ instruction parseInstr(WORD_32bit instr) {
     instruction tmpInstr;
     switch(getOpcode(instr)) {
         case op_funct:
-            tmpInstr = parseOpFunct(instr);
+            tmpInstr = parseOpRType(instr);
             break;
         case op_addi:
-            tmpInstr = parseOpAddi(instr);
-            break;
         case op_lw:
-            tmpInstr = parseOpLw(instr);
-            break;
         case op_lh:
-            tmpInstr = parseOpLh(instr);
-            break;
         case op_lhu:
-            tmpInstr = parseOpLhu(instr);
-            break;
         case op_lb:
-            tmpInstr = parseOpLb(instr);
-            break;
         case op_lbu:
-            tmpInstr = parseOpLbu(instr);
-            break;
         case op_sw:
-            tmpInstr = parseOpSw(instr);
-            break;
         case op_sh:
-            tmpInstr = parseOpSh(instr);
-            break;
         case op_sb:
-            tmpInstr = parseOpSb(instr);
-            break;
         case op_lui:
-            tmpInstr = parseOpLui(instr);
-            break;
         case op_andi:
-            tmpInstr = parseOpAndi(instr);
-            break;
         case op_ori:
-            tmpInstr = parseOpOri(instr);
-            break;
         case op_nori:
-            tmpInstr = parseOpNori(instr);
-            break;
         case op_slti:
-            tmpInstr = parseOpSlti(instr);
-            break;
         case op_beq:
-            tmpInstr = parseOpBeq(instr);
-            break;
         case op_bne:
-            tmpInstr = parseOpBne(instr);
+            tmpInstr = parseOpIType(instr);
             break;
         case op_j:
-            tmpInstr = parseOpJ(instr);
-            break;
         case op_jal:
-            tmpInstr = parseOpJal(instr);
+            tmpInstr = parseOpJType(instr);
             break;
         case op_halt:
-            tmpInstr = parseOpHalt(instr);
+            tmpInstr = parseOpSType(instr);
             break;
         default:
             break;
