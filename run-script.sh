9e21094118fe95b9a7b1db7de213b19cda5efd44
hydai <z54981220@gmail.com>
2014-03-11 11:18:11 +0800

    judge script

---
+++ run-script.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+echo "Start judge"
+cd archi40
+echo "Building simulator"
+cd simulator
+make clean
+make
+cd ..
+echo "Judging"
+mkdir judgingTemp
+cp simulator/single_cycle judgingTemp
+cp testcase/iimage.bin judgingTemp
+cp testcase/dimage.bin judgingTemp
+./judgingTemp/single_cycle
+echo "Compare"
+echo "Diff error_dump.rpt"
+diff testcase/error_dump.rpt judgingTemp/error_dump.rpt
+echo "Diff snapshot.rpt"
+diff testcase/snapshot.rpt judgingTemp/snapshot.rpt
