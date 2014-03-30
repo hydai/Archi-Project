5382f0a5269d152945cad2349f10a9ffc38523cb
Jack Force <jack1243star@gmail.com>
2014-03-30 12:11:25 +0800

    Translate register names and fix #1

--- mips-assembler/mips-as.lua
+++ mips-assembler/mips-as.lua
@@ -339,6 +339,42 @@ instruction = {
    end
 }
 
+-- MIPS register names
+register = {
+   ["$zero"] = "$0",
+   ["$at"] = "$1",
+   ["$v0"] = "$2",
+   ["$v1"] = "$3",
+   ["$a0"] = "$4",
+   ["$a1"] = "$5",
+   ["$a2"] = "$6",
+   ["$a3"] = "$7",
+   ["$t0"] = "$8",
+   ["$t1"] = "$9",
+   ["$t2"] = "$10",
+   ["$t3"] = "$11",
+   ["$t4"] = "$12",
+   ["$t5"] = "$13",
+   ["$t6"] = "$14",
+   ["$t7"] = "$15",
+   ["$s0"] = "$16",
+   ["$s1"] = "$17",
+   ["$s2"] = "$18",
+   ["$s3"] = "$19",
+   ["$s4"] = "$20",
+   ["$s5"] = "$21",
+   ["$s6"] = "$22",
+   ["$s7"] = "$23",
+   ["$t8"] = "$24",
+   ["$t9"] = "$25",
+   ["$k0"] = "$26",
+   ["$k1"] = "$27",
+   ["$gp"] = "$28",
+   ["$sp"] = "$29",
+   ["$fp"] = "$30",
+   ["$ra"] = "$31",
+}
+
 -- Return binary string representation of a 32-bit integer
 function binary(i, endian)
    local result = ""
@@ -375,14 +411,22 @@ function mipsas(inputfile, outputfile, startaddr)
    -- Instruction implementation
    local inst = instruction
 
-   -- Strip comment and empty lines
+   -- Strip comment and translate register name
    io.input(inputfile)
-   local src = io.tmpfile()
+   local srclist = {}
    for s in io.lines() do
       if not empty(s) then
-         src:write(string.match(s, "([^#]*)").."\n")
+         -- Strip comment
+         local stripped = string.match(s, "([^#]*)").."\n"
+         -- Translate register name
+         local t1 = string.gsub(stripped, "$zero", "$0")
+         local t2 = string.gsub(t1, "$%a%w", register)
+         table.insert(srclist, t2)
       end
    end
+   -- Write to temp file
+   local src = io.tmpfile()
+   for i,v in pairs(srclist) do src:write(v) end
    src:flush()
 
    -- Create label table
