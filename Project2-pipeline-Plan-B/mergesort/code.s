054cac766e21f6913c02bbe8938401be7ce9dd9f
hydai <z54981220@gmail.com>
2014-05-24 02:08:32 +0800

    Add load data of divide part

--- Project2-pipeline-Plan-B/mergesort/code.s
+++ Project2-pipeline-Plan-B/mergesort/code.s
@@ -19,27 +19,56 @@
 init:   ;; Initialize
         add     $t0,    $zero,  $zero
         addi    $t0,    $t0,    1
-        ;; Set $gp to 1
-        add     $gp,    $t0,    $zero
-        ;; Set $at to 0
+        add     $gp,    $t0,    $zero   ;; Set $gp to 1
         add     $at,    $gp,    $t0
-        sub     $at,    $at,    $at
-        ;; Set $v0 as array address 
-        add     $v0,    $at,    $at
-        ;; Set $v1 as array size
-        addi    $v1,    $v0,    32
+        sub     $at,    $at,    $at     ;; Set $at to 0
+        add     $v0,    $at,    $at     ;; Set $v0 as array address
+        addi    $v1,    $v0,    32      ;; Set $v1 as array size
         jal     mgs
-        ;; Exit
-        j       exit
+        j       exit                    ;; Goto exit
 
 mgs:    ;; Merge sort divide part
-        ;; TODO
+        beq     $v1,    $gp,    mgsre   ;; Check size of array <= 1 or not
+        sra     $a1,    $v1,    1       ;; lscSize = arrSize / 2
+        add     $t9,    $a1,    $zero
+        sll     $t9,    $t9,    2       ;; $t9 = size of memory
+        sub     $a3,    $v1,    $a1     ;; rscSize = arrSize - lscSize
+        add     $t8,    $a3,    $zero
+        sll     $t8,    $t8,    2       ;; $t8 = size of memory
+        add     $a0,    $sp,    $zero   ;; Address of lsc = $sp
+        add     $sp,    $sp,    $t9     ;; Alloc mem from stack for lsc
+        add     $a2,    $sp,    $zero   ;; Address of rsc = $sp
+        add     $sp,    $sp,    $t8     ;; Alloc mem from stack for rsc
+
+        ;; Load data to lsc
+        add     $t1,    $zero,  $zero
+        add     $t2,    $a0,    $zero
+        add     $t3,    $v0,    $zero
+ldLsc:  ;; Load lsc from memory
+        lw      $t4,    0($t3)
+        sw      $t4,    0($t2)
+        addi    $t3,    $t3,    4
+        addi    $t2,    $t2,    4
+        addi    $t1,    $t1,    1
+        bne     $a1,    $t1,    ldLsc
+
+        ;; Load data to rsc
+        add     $t1,    $zero,  $zero
+        add     $t2,    $a2,    $zero
+ldRsc:  ;; Load rsc from memory
+        lw      $t4,    0($t3)
+        sw      $t4,    0($t2)
+        addi    $t3,    $t3,    4
+        addi    $t2,    $t2,    4
+        addi    $t1,    $t1,    1
+        bne     $a3,    $t1,    ldRsc
+        ;; TODO: recursize call merge sort and merge
 
 mgsre:  ;; Return
         jr      $ra
 
 mg:     ;; Merge conquer part
-        ;; TODO
+        ;; TODO: merge
 
 exit:   ;; Exit pointer
         halt
